8b2ae900c4d71547d6cee0dceddcc9e0057a0724
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-10-15 17:38:11 +0200

    A size attribute containing length-1 is misleading
    String.length() gets inlined so it's completely useless.
    Source: http://stackoverflow.com/a/6626911/6538637

--- src/sequencer/Complementary.java
+++ src/sequencer/Complementary.java
@@ -5,20 +5,20 @@ package sequencer;
  */
 
 public class Complementary extends Sequence {
-	private final int size;
-
 	/**
 	 * @param initial initial fragment, not the complementary
 	 * @param first the "normal" fragment
 	 */
 	public Complementary(String initial, Sequence first){
 		super(initial, first);
-		size = fragment.length()-1;
 	}
 
+	/**
+	 * Read a nucleotide from the end of the original sequence, complementing it.
+	 */
 	@Override
 	public char get(int i){
-		char c = fragment.charAt(size-i);
+		char c = fragment.charAt(fragment.length()-i-1);
 		switch(c){
 			case 'a': return 't';
 			case 't': return 'a';
