f398dbab2202fa66682a3e46ce30456bdbec9e3b
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-12-07 22:36:05 +0100

    English

--- src/sequencer/AlignmentPath.java
+++ src/sequencer/AlignmentPath.java
@@ -1,21 +1,22 @@
 package sequencer;
 /**
- * A data structure containing the score of an alignment and the path in the alignment matrix to retrace the alignement
+ * A data structure containing the score of an alignment and the path in the alignment matrix to retrace the alignment
  * @author Guillaume Huysmans, Jason Bury
  */
 public class AlignmentPath{
 	/** The score of the alignment*/
 	public final int score;
 	/**
-	 * The position on the last line where the path starts in the alignement matrix.
-	 * Yes, it starts always from the last line.
-	 * It is also the position on the second sequence, -indexed from 1-, where the alignement finish.
+	 * The position on the last line where the path starts in the alignment
+	 * matrix (it always starts from the last line).
+	 * It is also the position on the second sequence - indexed from 1 - where
+	 * the alignment finishes.
 	 */
 	public final int start;
 	/**
 	 * The position where starts the alignment. Indexed from 1!
-	 * If its positive, the alignement starts at the (position)th symbol from the first sequence
-	 * But if its negative, its in case where the first sequence is included in the second, the alignement starts at the (-position)th symbol from the second sequence
+	 * If its positive, the alignment starts at the (position)th symbol from the first sequence
+	 * But if its negative, its in case where the first sequence is included in the second, the alignment starts at the (-position)th symbol from the second sequence
 	 */
 	public final int delta;
 
@@ -29,7 +30,7 @@ public class AlignmentPath{
 	/**
 	 * @param score The score of the semi-global alignment where the suffix of the first sequence is aligned with the prefix of the second.
 	 * @param start The position on the last line where starts the alignment int the matrix.
-	 * @param delta The position on the first sequence where starts the alignement.
+	 * @param delta The position on the first sequence where starts the alignment.
 	 * @param path The path.
 	 * @param pathSize The length of the path.
 	 */
